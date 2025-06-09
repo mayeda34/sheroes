6cf762c3d264c1028b0e9bf2efff07f0be2acf92
mayeda34 <mayeda346@gmail.com>
2025-06-09 23:58:35 +0500

    Add files via upload

---
+++ jobseeker-navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="jobseeker-dashboard.php">
+            <i class="fas fa-users"></i> SHEROES
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="jobseeker-dashboard.php">
+                        <i class="fas fa-tachometer-alt"></i> Dashboard
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="browse-jobs.php">
+                        <i class="fas fa-search"></i> Browse Jobs
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="my-applications.php">
+                        <i class="fas fa-file-alt"></i> My Applications
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile.php">
+                        <i class="fas fa-user"></i> My Profile
+                    </a>
+                </li>
+            
+                
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="logout.php">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
